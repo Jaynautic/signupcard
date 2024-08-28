6b0d8c031d54c1c71a43424814f9de7b41e5ce4f
Jaynautic <jaynauticstudios@gmail.com>
2024-08-28 16:40:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var signupSideButton = document.getElementById('signupSideButton');
+var signinButton = document.getElementById('signinButton');
+var cardContainer = document.querySelector('.card-container');
+var sideCardInner = document.querySelector('.side-card-inner');
 
+
+signupSideButton.addEventListener("click", switchSides);
+
+function switchSides() {
+    sideCardInner.classList.add('move')
+}
